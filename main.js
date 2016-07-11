1826975751bf4d5cac52ff1839473d9ac755577b
banyudu <banyudu@gmail.com>
2016-07-11 17:01:14 +0800

    add shortcut to show dev tools window

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ app.on('window-all-closed', () => {
   }
 })
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
   // and load the index.html of the app.
   mainWindow.loadURL(path.join('file://', __dirname, 'index.html'))
@@ -34,6 +34,34 @@ function createWindow () {
   })
 
   Menu.setApplicationMenu(Menu.buildFromTemplate(menuTpl))
+
+  // For some reason, Electron shortcuts are registered
+  // globally, which means that the app listers for shorcuts
+  // even if its not currently focused, potentially interferring
+  // with shorcuts registered by other applications.
+  // As a workaround, we register all shortcuts when the windows
+  // gains focus, and unregister them when the windows loses focus.
+  // See http://electron.atom.io/docs/api/global-shortcut/
+
+  mainWindow.on('focus', () => {
+    electron.globalShortcut.register('CmdOrCtrl+Alt+I', () => {
+      mainWindow.webContents.openDevTools({
+        mode: 'undocked'
+      });
+    });
+  });
+
+  mainWindow.on('blur', () => {
+    electron.globalShortcut.unregisterAll();
+  });
+
+  // Prevent external resources from being loaded (like images)
+  // when dropping them on the WebView.
+  // See https://github.com/electron/electron/issues/5919
+  mainWindow.webContents.on('will-navigate', (event) => {
+    event.preventDefault();
+  });
+
 }
 
 // This method will be called when Electron has done everything

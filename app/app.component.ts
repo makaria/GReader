93abc1490bceda404380b055ad03289df7123f31
banyudu <banyudu@gmail.com>
2016-07-15 10:57:38 +0800

    revert changes about removing angular from project

--- app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import {ReaderComponent} from './reader.component'
-@Component({
-  selector: 'greader-app',
-  template: '<greader-reader></greader-reader>',
-  directives: [ReaderComponent]
-})
-export class AppComponent { }
--- app/content.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'greader-content',
-  template: 'content'
-})
-export class ContentComponent { }

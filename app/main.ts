b70f04f3a7156f71071aeb27fb3d8f158b73ad10
banyudu <banyudu@gmail.com>
2016-07-19 11:09:35 +0800

    modify tslint rules and apply

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,3 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './components/app.component';
-bootstrap(AppComponent);
+import { bootstrap }    from '@angular/platform-browser-dynamic'
+import { AppComponent } from './components/app.component'
+bootstrap(AppComponent)

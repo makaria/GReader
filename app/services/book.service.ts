5f86fde12781e84005657a47244cdb0f9ab09d36
班玉都 <banyudu@icloud.com>
2016-07-22 19:00:43 +0800

    use interfaces instead of models in service

--- app/services/book.service.ts
+++ app/services/book.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core'
-import {BookShelfModel} from '../models/bookshelf.model'
-import {BookModel} from '../models/book.model'
+import { BookShelfModel } from '../models/bookshelf.model'
+import { BookModel } from '../models/book.model'
+import { BookShelfInterface } from '../interfaces/bookshelf.interface'
+import { BookInterface } from '../interfaces/book.interface'
 @Injectable()
 export class BookService {
-  getBookShelfs(): [BookShelfModel] {
+  getBookShelfs(): [BookShelfInterface] {
       let a: BookShelfModel
       return [a]
   }
 
-  getCurrentBook(): BookModel {
+  getCurrentBook(): BookInterface {
       let b: BookModel
       return b
   }
